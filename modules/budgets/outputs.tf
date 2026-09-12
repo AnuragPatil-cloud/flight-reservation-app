4c4f05befc297f83507d91865c1e5a5bcb4547a7
Anurag Patil <patilanurag1703@gmail.com>
2026-09-13 02:20:09 +0530

    Delete modules directory

--- modules/budgets/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "budget_name" {
-  value = azurerm_consumption_budget_subscription.this.name
-}
-
-output "budget_id" {
-  value = azurerm_consumption_budget_subscription.this.id
-}
--- modules/storage/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "storage_account_name" {
-  value = azurerm_storage_account.this.name
-}
-
-output "storage_container_name" {
-  value = azurerm_storage_container.this.name
-}
