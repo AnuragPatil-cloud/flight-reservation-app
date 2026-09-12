42638f4c38e386aee657df615eac777462c8bc01
Anurag Patil <anurag.patil.devops@gmail.com@flight-reservation-application-monitoring-vm.blqqlq5rusiezdelzqyclwls0f.vx.internal.cloudapp.net>
2026-09-12 13:28:07 +0000

    Update contact details

--- frontend/src/Components/Contact.jsx
+++ frontend/src/Components/Contact.jsx
@@ -1,32 +1,60 @@
-import { toast, ToastContainer } from "react-toastify";
+import React from "react";
 
-export default function Contact() {
+const Contact = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert("Email sent");
+  };
 
-    const mailSent = (e) => {
-        e.preventDefault();
-        toast("Email sent")
-    };
-    return <>
-        <ToastContainer />
-        <div className="contact">
-            <div className="row">
-                <div className="col-md-5">
-                    <h2>Contact Us</h2>
-                    <br />
-                    <p><i className='bx bxs-map'></i>&nbsp;&nbsp;Vidhya Nagar, Shivganga Tower, fourth floor</p>
-                    <p><i className='bx bxs-phone'></i>&nbsp;&nbsp;9754947747</p>
-                    <p><i className='bx bxs-envelope'></i>&nbsp;&nbsp;shivamlowanshi@gmail.com</p>
-                </div>
-                <div className="col-md-6">
-                    <h2>Stay In Touch</h2>
-                    <br />
-                    <input type="text" placeholder="Enter Your Email Address" />
-                    <button onClick={mailSent}><i className='bx bx-right-arrow-alt'></i></button>&nbsp;
-                    <br /><br />
-                    <p>Enter your email address for promotions, news and updates.</p>
-                    <p></p>
-                </div>
-            </div>
+  return (
+    <div className="contact">
+      <div className="row">
+        <div className="col-md-5">
+          <h2>Contact Us</h2>
+          <br />
+
+          <p>
+            <i className="bx bxs-map"></i>
+            {"  Katraj, Near Katraj Talawa, Katraj"}
+          </p>
+
+          <p>
+            <i className="bx bxs-phone"></i>
+            {"  9226531573"}
+          </p>
+
+          <p>
+            <i className="bx bxs-envelope"></i>
+            {"  anurag.patil.devops@gmail.com"}
+          </p>
         </div>
-    </>
-}
+
+        <div className="col-md-6">
+          <h2>Stay In Touch</h2>
+          <br />
+
+          <form onSubmit={handleSubmit}>
+            <input
+              type="email"
+              className="form-control"
+              placeholder="Enter your email address"
+              required
+            />
+
+            <br />
+
+            <button type="submit" className="btn btn-primary">
+              Subscribe
+            </button>
+          </form>
+
+          <p>
+            Enter your email address for promotions, news and updates.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
