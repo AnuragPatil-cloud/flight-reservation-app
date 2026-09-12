c4af4c7add9a713b12602b8d634a4e6eea1466ff
Anurag Patil <patilanurag1703@gmail.com>
2026-09-12 18:05:22 +0530

    Add files via upload

---
+++ frontend/src/Components/Footer.jsx
@@ -0,0 +1,13 @@
+export default function () {
+  return (
+    <>
+      <footer>
+        <div className="container">
+          <p>
+            &copy; 2026 AnuragPatil-cloud Flight Reservation System. All rights reserved.
+          </p>
+        </div>
+      </footer>
+    </>
+  );
+}
